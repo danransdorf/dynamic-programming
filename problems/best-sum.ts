82c8c03b14fa612df432318f542c307033549aa9
MonteBro <czech.illusion@gmail.com>
2024-03-14 15:06:48 +0100

    Best sum - tabulation

--- problems/best-sum.ts
+++ problems/best-sum.ts
@@ -32,7 +32,7 @@ measure(() => bestSumBasic(20, [1, 3, 5, 7, 9]));
 measure(() => bestSumBasic(7, [5, 3, 4, 7]));
 measure(() => bestSumBasic(250, [7, 14]));
 
-const bestSum = (
+const bestSumMemo = (
   targetSum: number,
   numbers: number[],
   memo: Record<number, number[] | null> = {}
@@ -44,7 +44,7 @@ const bestSum = (
   let bestCombination: number[] | null = null;
   for (const num of numbers) {
     const newTarget = targetSum - num;
-    const res = bestSum(newTarget, numbers, memo);
+    const res = bestSumMemo(newTarget, numbers, memo);
     if (res) {
       const combination = [num, ...res];
       if (!bestCombination || combination.length < bestCombination?.length) {
@@ -58,8 +58,43 @@ const bestSum = (
 };
 
 console.log("DP - memo ↓");
-measure(() => bestSum(20, [1, 3, 5, 7, 9]));
-measure(() => bestSum(7, [5, 3, 4, 7]));
-measure(() => bestSum(250, [7, 14]));
-measure(() => bestSum(15001, [2, 4, 6, 8, 10, 12, 14, 16]));
-measure(() => bestSum(15001, [1, 2, 3, 4, 5, 6, 7, 8, 16, 32, 64, 128]));
+measure(() => bestSumMemo(20, [1, 3, 5, 7, 9]));
+measure(() => bestSumMemo(7, [5, 3, 4, 7]));
+measure(() => bestSumMemo(250, [7, 14]));
+measure(() => bestSumMemo(15001, [2, 4, 6, 8, 10, 12, 14, 16]));
+measure(() => bestSumMemo(15001, [1, 2, 3, 4, 5, 6, 7, 8, 16, 32, 64, 128]));
+
+// Tabulation
+const bestSumTable = (
+  targetSum: number,
+  numbers: number[]
+): number[] | null => {
+  const table: ([number, number] | null)[] = Array(targetSum + 1).fill(null); // [stepSize, currentDepth]
+  table[0] = [1, 0];
+
+  for (let i = 0; i < targetSum + 1; i++) {
+    if (table[i]) {
+      for (const num of numbers) {
+        if (!table[i + num] || table[i + num]![1] > table[i]![1] + 1) {
+          table[i + num] = [num, table[i]![1] + 1];
+        }
+      }
+    }
+  }
+  if (table[targetSum] === null) return null;
+
+  const res = [];
+  for (let i = targetSum; i > 0; null) {
+    res.push(table[i]![0]);
+    i -= table[i]![0];
+  }
+
+  return res;
+};
+
+console.log("DP - table ↓");
+measure(() => bestSumTable(20, [1, 3, 5, 7, 9]));
+measure(() => bestSumTable(7, [5, 3, 4, 7]));
+measure(() => bestSumTable(250, [7, 14]));
+measure(() => bestSumTable(15001, [2, 4, 6, 8, 10, 12, 14, 16]));
+measure(() => bestSumTable(15001, [1, 2, 3, 4, 5, 6, 7, 8, 16, 32, 64, 128]));
