b1ef1d559ff9e80600eae1598b7ee14757ce333a
MonteBro <czech.illusion@gmail.com>
2024-03-14 09:44:08 +0100

    Grid traveler - memoization

--- README.md
+++ README.md
@@ -1,15 +1,12 @@
 # dynamic-programming
 
-To install dependencies:
-
-```bash
-bun install
-```
-
-To run:
-
-```bash
-bun run index.ts
-```
-
-This project was created using `bun init` in bun v1.0.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+## NOTES
+### Memoization recipe - Alvin's memoization recipe
+1. Make it work
+  - visualize the problem as a tree
+  - implement using recursion
+  - test it
+2. Make it efficient
+  - recognize repeated actions
+  - store in a memo object
+  - add recursion base case to return memo value
---
+++ problems/grid-traveler.ts
@@ -0,0 +1,33 @@
+import { measure } from "../utils/measure";
+
+const gridTravelerBasic = (w: number, h: number): number =>
+  w == 0 || h == 0
+    ? 0
+    : w == 1 || h == 1
+    ? 1
+    : gridTravelerBasic(w - 1, h) + gridTravelerBasic(w, h - 1);
+
+measure(() => gridTravelerBasic(10, 10));
+measure(() => gridTravelerBasic(10, 15));
+measure(() => gridTravelerBasic(17, 15));
+
+// Memo
+const gridTraveler = (
+  w: number,
+  h: number,
+  memo: Record<string, number> = {}
+): number => {
+  if (w == 0 || h == 0) return 0;
+  if (w == 1 || h == 1) return 1;
+
+  const key = Math.min(w, h) + "," + Math.max(w, h);
+  if (key in memo) return memo[key];
+
+  memo[key] = gridTraveler(w - 1, h, memo) + gridTraveler(w, h - 1, memo);
+  return memo[key];
+};
+
+measure(() => gridTraveler(10, 10));
+measure(() => gridTraveler(10, 15));
+measure(() => gridTraveler(17, 15));
+measure(() => gridTraveler(500, 500));
